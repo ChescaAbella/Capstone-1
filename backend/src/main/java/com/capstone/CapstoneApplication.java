2e2905236e8b2771c66818fdc1d7e120b51169d9
cm6322 <cheltermatthew.gilbuena@cit.edu>
2026-01-06 16:24:07 +0800

    Added temporary code for debugging

--- backend/src/main/java/com/capstone/CapstoneApplication.java
+++ backend/src/main/java/com/capstone/CapstoneApplication.java
@@ -3,9 +3,16 @@ package com.capstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CapstoneApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapstoneApplication.class, args);
+    }
+    @PostConstruct
+    public void debugDb() {
+        System.out.println("DB URL = " + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println("DB USER = " + System.getenv("SPRING_DATASOURCE_USERNAME"));
     }
 }
